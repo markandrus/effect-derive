a1d3a13207fc26b4c84734c84b1b75c1b51a8d03
Mark Roberts <markandrusroberts@gmail.com>
2024-11-19 13:33:22 +0100

    test: add fromArray implemented using ana

--- test/index.ts
+++ test/index.ts
@@ -8,9 +8,19 @@ function project<A> (list: List<A>): ListF<A, List<A>> {
   return list
 }
 
+function embed<A> (listF: ListF<A, List<A>>): List<A> {
+  return listF
+}
+
 function cata<A, B> (f: (listF: ListF<A, B>) => B): (list: List<A>) => B {
-  return function c(list: List<A>): B {
-    return f(listFCovariant.map(project(list), c))
+  return function go(list: List<A>): B {
+    return f(listFCovariant.map(project(list), go))
+  }
+}
+
+function ana<A, B> (g: (b: B) => ListF<A, B>): (b: B) => List<A> {
+  return function go(b: B): List<A> {
+    return embed(listFCovariant.map(g(b), go))
   }
 }
 
@@ -23,6 +33,16 @@ function toArray<A> (list: List<A>): A[] {
         return [listF.head].concat(listF.tail)
     }
   })(list)
+}
+
+function fromArray<A> (as: A[]): List<A> {
+  return ana<A, A[]>(as => {
+    if (as.length === 0) {
+      return { type: 'Nil' }
+    } else {
+      return { type: 'Cons', head: as[0], tail: as.slice(1) }
+    }
+  })(as)
 }
 
 test('toArray implemented using cata', () => {
@@ -43,5 +63,26 @@ test('toArray implemented using cata', () => {
   }
   const actual = toArray(list)
   const expected = [1, 2, 3]
+  assert.deepStrictEqual(actual, expected)
+})
+
+test('fromArray implemented using ana', () => {
+  const as = [1, 2, 3]
+  const actual = fromArray(as)
+  const expected: List<number> = {
+    type: 'Cons',
+    head: 1,
+    tail: {
+      type: 'Cons',
+      head: 2,
+      tail: {
+        type: 'Cons',
+        head: 3,
+        tail: {
+          type: 'Nil'
+        }
+      }
+    }
+  }
   assert.deepStrictEqual(actual, expected)
 })
